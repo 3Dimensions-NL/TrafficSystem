ff3ed013e082d6951022f423b2c25be103ce13e3
Tony van Loon <tony@3dimensions.nl>
2024-10-22 11:26:57 +0200

    Debug speed text added to lanes

--- Runtime/TrafficLane.cs
+++ Runtime/TrafficLane.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.Splines;
 namespace _3Dimensions.TrafficSystem.Runtime
@@ -235,6 +236,14 @@ namespace _3Dimensions.TrafficSystem.Runtime
                     }
                 }
             }
+
+            if (waypoints.Count > 0)
+            {
+                Vector3 pos = waypoints[0].transform.position + new Vector3(0, TrafficManager.Instance.gizmosHeight, 0);
+                DrawString(speed.ToString(CultureInfo.InvariantCulture), pos, Color.yellow);
+                // Gizmos.color = Color.green;
+                // Gizmos.DrawSphere(pos, 0.1f);
+            }
         }
 
         private void OnDrawGizmosSelected()
@@ -255,6 +264,28 @@ namespace _3Dimensions.TrafficSystem.Runtime
                     }
                 }
             }
+        }
+        
+        private void DrawString(string text, Vector3 worldPos, Color? colour = null) {
+            UnityEditor.Handles.BeginGUI();
+
+            var restoreColor = GUI.color;
+
+            if (colour.HasValue) GUI.color = colour.Value;
+            var view = UnityEditor.SceneView.currentDrawingSceneView;
+            Vector3 screenPos = view.camera.WorldToScreenPoint(worldPos);
+
+            if (screenPos.y < 0 || screenPos.y > Screen.height || screenPos.x < 0 || screenPos.x > Screen.width || screenPos.z < 0)
+            {
+                GUI.color = restoreColor;
+                UnityEditor.Handles.EndGUI();
+                return;
+            }
+
+            Vector2 size = GUI.skin.label.CalcSize(new GUIContent(text));
+            GUI.Label(new Rect(screenPos.x - (size.x / 2), Screen.height - screenPos.y - (size.y * 4), size.x, size.y), text);
+            GUI.color = restoreColor;
+            UnityEditor.Handles.EndGUI();
         }
 #endif
     }
