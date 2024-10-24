dc93aa925b3a9e940790f147669f125b2f2a7f26
Tony van Loon <tony@3dimensions.nl>
2024-10-24 14:31:16 +0200

    Namespace fix for TrafficSurface

--- Runtime/TrafficSurface.cs
+++ Runtime/TrafficSurface.cs
@@ -1,21 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-namespace _3Dimensions
+namespace _3Dimensions.TrafficSystem.Runtime
 {
     public class TrafficSurface : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
--- Runtime/VehicleWheel.cs
+++ Runtime/VehicleWheel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 namespace _3Dimensions.TrafficSystem.Runtime
 {
     public class VehicleWheel : MonoBehaviour
